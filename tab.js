282666430341546c2baca746e9a9225fae13bcc5
baeggoose <bsj154@gmail.com>
2023-07-26 21:26:07 +0900

    이벤트 버블링 응용, dataset

--- tab.js
+++ tab.js
@@ -1,11 +1,10 @@
-var tab = $(".tab-button");
-var content = $(".tab-content");
-for (let i = 0; i < tab.length; i++) {
-  tab.eq(i).on("click", function () {
-    tabShow(i);
-  });
-}
+$(".list").click(function (e) {
+  tabShow(e.target.dataset.id);
+});
+
 function tabShow(k) {
+  var tab = $(".tab-button");
+  var content = $(".tab-content");
   tab.removeClass("orange");
   tab.eq(k).addClass("orange");
   content.removeClass("show");
