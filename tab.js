04565e2162b458c67323c5575768107eb054635d
baeggoose <bsj154@gmail.com>
2023-07-26 19:01:22 +0900

    tab.js 탭기능 for문으로 구현

--- tab.js
+++ tab.js
@@ -1,3 +1,10 @@
-$(".tab-button")
-  .eq(0)
-  .on("click", function () {});
+var tab = $(".tab-button");
+var content = $(".tab-content");
+for (let i = 0; i < tab.length; i++) {
+  tab.eq(i).on("click", function () {
+    tab.removeClass("orange");
+    tab.eq(i).addClass("orange");
+    content.removeClass("show");
+    content.eq(i).addClass("show");
+  });
+}
